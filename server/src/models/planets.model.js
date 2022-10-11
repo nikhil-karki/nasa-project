43b79fcf9339359bb2bc4c11fd177e2834d56faf
nikhil-karki <nikhil@pipesort.com>
2022-10-11 21:59:42 +0530

    Add model to get planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,40 @@
-const planets = [];
+const parse = require("csv-parser");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+// node doesn't wait for streams to complete async behaviour challenge
+
+fs.createReadStream("data/kepler_data.csv").pipe(
+  parse({
+    comment: "#",
+    columns: true,
+  })
+    .on("data", (data) => {
+      if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+      }
+    })
+    .on("error", (err) => {
+      console.log("error", err);
+    })
+    .on("end", () => {
+      console.log(
+        habitablePlanets.map((planet) => {
+          return planet["kepler_name"];
+        })
+      );
+      console.log(`${habitablePlanets.length} habitable planets found!`);
+    })
+);
+
+module.exports = { planets: habitablePlanets };
